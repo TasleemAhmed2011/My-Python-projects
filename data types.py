a4b13946411c02360916aefefd296e1ce6f02f5d
TasleemAhmed2011 <tasleemahmed2011@gmail.com>
2025-05-27 20:55:46 +0500

    basic code

---
+++ data types.py
@@ -0,0 +1,9 @@
+var1="Ahmed"
+var2=40
+var3=40.6
+var4= True
+var11=type(var1)
+var22=type(var2)
+var33=type(var3)
+var44=type(var4)
+print("\nAhmed is a " + str(var11) + "." + "\n40 is a " + str(var22) + "." + "\n40.6 is a "+str(var33)+"." + "\nAhmed is my father that is  " + str(var44) + ".")
---
+++ escape sequences.py
@@ -0,0 +1,9 @@
+print("Hello WORLD")
+age=int(input("Enter your age"))
+name=input("Enter your name")
+print("\nHello. " +  "\nYour are "+ str(age) + " years old. " + "\nAnd your name is "+ name)
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+myprof=("Software Engineer")
+print(len(myprof))
