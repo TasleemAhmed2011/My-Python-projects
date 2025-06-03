cf3c07468236d22fdcb60c586c6178d648effd1c
TasleemAhmed2011 <tasleemahmed2011@gmail.com>
2025-06-03 20:47:45 +0500

    conditional statements

--- conditional statements.py
+++ conditional statements.py
@@ -7,3 +7,14 @@ if var1 < 18:
 else:
     print("You can vote as you are above 18.")
 
+print("Profit and Loss Calculator")
+cprice = float(input("Enter the cost price of the item: "))
+sprice = float(input("Enter the selling price of the item: "))
+
+if sprice > cprice:
+    print("Profit:", sprice - cprice)
+elif sprice < cprice:
+    print("Loss:", cprice - sprice)
+else:
+    print("No Profit No Loss")
+
