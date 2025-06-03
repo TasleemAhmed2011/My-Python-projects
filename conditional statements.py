542cb9447f771e8872abe8a45988afb4764be6b7
TasleemAhmed2011 <tasleemahmed2011@gmail.com>
2025-06-03 20:43:44 +0500

    conditional statements

---
+++ conditional statements.py
@@ -0,0 +1,9 @@
+var1=int(input("Enter your age: "))
+if var1 < 18:
+    print("You cannot drive any vehicle as you have not got a licence.")
+
+if var1 < 18:
+    print("You cannot vote as you are under 18.")
+else:
+    print("You can vote as you are above 18.")
+
