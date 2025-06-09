9cce6ba7465281f18d29050eb40949a6fb5d361e
TasleemAhmed2011 <tasleemahmed2011@gmail.com>
2025-06-09 10:26:22 +0500

    data types

--- data types.py
+++ data types.py
@@ -6,13 +6,13 @@ var11=type(var1)
 var22=type(var2)
 var33=type(var3)
 var44=type(var4)
-print("\nAhmed is a " + str(var11) + "." + "\n40 is a " + str(var22) + "." + "\n40.6 is a "+str(var33)+"." + "\nAhmed is my father that is  " + str(var44) + ".")
+print("\nAhmed is a " + str(var11) + "." + "\n40 is a " + str(var22) + "." + "\n40.6 is a "+str(var33)+"." + "\nAhmed is my father that is true  " + str(var44) + ".")
 print("Indexes")
 str=input("\nEnter a string: ")
 print("The string you entered is: " + str)
 print(len(str))
-print(str[::   -1])  
-print(str[::   3]) #
-print(str[:0]) 
-print(str[0:5])
-print(str[5:])
+print(str[::   -1])  # Reverses the string
+print(str[::   3]) # Prints every 3rd character
+print(str[:0])  # Prints an empty string
+print(str[0:5])  # Prints the first 5 characters
+print(str[5:])  # Prints the string from the 5th character to the end
