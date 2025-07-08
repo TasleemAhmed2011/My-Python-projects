c8f63c50f5a9e95512e28d6dd386db4eed81295d
TasleemAhmed2011 <tasleemahmed2011@gmail.com>
2025-07-08 20:58:31 +0500

    basic code

--- Basic code.py
+++ Basic code.py
@@ -3,3 +3,7 @@ d=input("Enter your Name ")
 e=int(input("Enter your age "))
 print("Your Name and Age is ",d,"and ",e)
 print("Thanks")
+
+password="BL5KSOV"
+real_pasword=password.lower()
+print(real_pasword)
